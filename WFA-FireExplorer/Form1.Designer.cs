a9090597ac0dcbe003e8479731fd954be7283c25
Chau Han <chaugiahan001@gmail.com>
2025-05-26 23:02:52 +0700

    Add treeView folder and Listview

--- WFA-FireExplorer/Form1.Designer.cs
+++ WFA-FireExplorer/Form1.Designer.cs
@@ -29,6 +29,14 @@
         private void InitializeComponent()
         {
             this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.btn_back = new System.Windows.Forms.Button();
+            this.btn_next = new System.Windows.Forms.Button();
+            this.btn_open = new System.Windows.Forms.Button();
+            this.btn_go = new System.Windows.Forms.Button();
+            this.lbl_path = new System.Windows.Forms.Label();
+            this.txt_path = new System.Windows.Forms.TextBox();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.listView1 = new System.Windows.Forms.ListView();
             this.SuspendLayout();
             // 
             // webBrowser1
@@ -37,24 +45,120 @@
             this.webBrowser1.Location = new System.Drawing.Point(0, 0);
             this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
             this.webBrowser1.Name = "webBrowser1";
-            this.webBrowser1.Size = new System.Drawing.Size(800, 450);
+            this.webBrowser1.Size = new System.Drawing.Size(623, 438);
             this.webBrowser1.TabIndex = 0;
+            // 
+            // btn_back
+            // 
+            this.btn_back.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_back.Location = new System.Drawing.Point(39, 33);
+            this.btn_back.Name = "btn_back";
+            this.btn_back.Size = new System.Drawing.Size(35, 23);
+            this.btn_back.TabIndex = 1;
+            this.btn_back.Text = "<";
+            this.btn_back.UseVisualStyleBackColor = true;
+            this.btn_back.Click += new System.EventHandler(this.btn_back_Click);
+            // 
+            // btn_next
+            // 
+            this.btn_next.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_next.Location = new System.Drawing.Point(79, 33);
+            this.btn_next.Name = "btn_next";
+            this.btn_next.Size = new System.Drawing.Size(35, 23);
+            this.btn_next.TabIndex = 2;
+            this.btn_next.Text = ">";
+            this.btn_next.UseVisualStyleBackColor = true;
+            this.btn_next.Click += new System.EventHandler(this.btn_next_Click);
+            // 
+            // btn_open
+            // 
+            this.btn_open.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_open.Location = new System.Drawing.Point(464, 33);
+            this.btn_open.Name = "btn_open";
+            this.btn_open.Size = new System.Drawing.Size(113, 23);
+            this.btn_open.TabIndex = 3;
+            this.btn_open.Text = "Open Manually";
+            this.btn_open.UseVisualStyleBackColor = true;
+           
+            // 
+            // btn_go
+            // 
+            this.btn_go.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_go.Location = new System.Drawing.Point(416, 33);
+            this.btn_go.Name = "btn_go";
+            this.btn_go.Size = new System.Drawing.Size(42, 23);
+            this.btn_go.TabIndex = 4;
+            this.btn_go.Text = "Go";
+            this.btn_go.UseVisualStyleBackColor = true;
+            
+            // 
+            // lbl_path
+            // 
+            this.lbl_path.AutoSize = true;
+            this.lbl_path.Location = new System.Drawing.Point(134, 38);
+            this.lbl_path.Name = "lbl_path";
+            this.lbl_path.Size = new System.Drawing.Size(32, 13);
+            this.lbl_path.TabIndex = 5;
+            this.lbl_path.Text = "Path:";
+            // 
+            // txt_path
+            // 
+            this.txt_path.Location = new System.Drawing.Point(172, 33);
+            this.txt_path.Name = "txt_path";
+            this.txt_path.Size = new System.Drawing.Size(223, 20);
+            this.txt_path.TabIndex = 7;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(39, 82);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(121, 310);
+            this.treeView1.TabIndex = 8;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(183, 82);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(394, 310);
+            this.listView1.TabIndex = 9;
+            this.listView1.UseCompatibleStateImageBehavior = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(623, 438);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.txt_path);
+            this.Controls.Add(this.lbl_path);
+            this.Controls.Add(this.btn_go);
+            this.Controls.Add(this.btn_open);
+            this.Controls.Add(this.btn_next);
+            this.Controls.Add(this.btn_back);
             this.Controls.Add(this.webBrowser1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
+
+            this.Load += new System.EventHandler(this.Form1_Load);
 
         }
 
         #endregion
 
         private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button btn_back;
+        private System.Windows.Forms.Button btn_next;
+        private System.Windows.Forms.Button btn_open;
+        private System.Windows.Forms.Button btn_go;
+        private System.Windows.Forms.Label lbl_path;
+        private System.Windows.Forms.TextBox txt_path;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ListView listView1;
     }
 }
 
--- WFA-FireExplorer/Form1.cs
+++ WFA-FireExplorer/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,163 @@ namespace WFA_FireExplorer
 {
     public partial class Form1 : Form
     {
+        private List<string> navHistory = new List<string>();
+        private int currHistoryIdx = -1;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string homePath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile); // "C:\Users\<Username>"
+            string startupPath = @"C:\";
+
+            LoadDirTree(homePath);     // TreeView shows user home
+            NavigateTo(startupPath);         // ListView shows C:\
+        }
+
+
+        private void LoadDirTree(string rootPath)
+        {
+            treeView1.Nodes.Clear();
+            try
+            {
+                var rootDirInfo = new DirectoryInfo(rootPath);
+                treeView1.Nodes.Add(CreateDirNode(rootDirInfo));
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error loading directory tree: {ex.Message}");
+            }
+        }
+
+        private TreeNode CreateDirNode(DirectoryInfo dirInfo)
+        {
+            var node = new TreeNode(dirInfo.Name) { Tag = dirInfo.FullName };
+            try
+            {
+                foreach (var dir in dirInfo.GetDirectories())
+                {
+                    node.Nodes.Add(CreateDirNode(dir));
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                // Handle access denied exceptions
+                node.Nodes.Add(new TreeNode("Access Denied") { ForeColor = Color.Red });
+            }
+            return node;
+        }
+
+        private void NavigateTo(string path)
+        {
+            if (currHistoryIdx >= 0 && currHistoryIdx < navHistory.Count - 1 && 
+                navHistory[currHistoryIdx] == path)  { 
+                
+                return; // Already at the current path
+            }
+
+            // Trim forward history if navigating to a new path
+            if (currHistoryIdx < navHistory.Count - 1)
+            {
+                navHistory = navHistory.Take(currHistoryIdx + 1).ToList();
+            }
+
+            navHistory.Add(path);
+            currHistoryIdx = navHistory.Count - 1;
+
+            txt_path.Text = path;
+            LoadFileList(path);
+        }
+
+        //private void btn_open_Click(object sender, EventArgs e)
+        //{
+        //    using (FolderBrowserDialog fbd = new FolderBrowserDialog())
+        //    {
+        //        fbd.Description = "Select a folder to explore";
+        //        if (fbd.ShowDialog() == DialogResult.OK)
+        //        {
+        //            webBrowser1.Url = new Uri(fbd.SelectedPath);
+        //            txt_path.Text = fbd.SelectedPath;
+        //            // Here you can add code to handle the selected folder
+        //            MessageBox.Show($"Selected folder: {fbd.SelectedPath}");
+        //        }
+        //    }
+        //}
+
+        private void btn_back_Click(object sender, EventArgs e)
+        {
+            if (currHistoryIdx > 0)
+            {
+                currHistoryIdx--;
+                string path = navHistory[currHistoryIdx];
+                txt_path.Text = path;
+                LoadFileList(path);
+            }
+        }
+
+        private void btn_next_Click(object sender, EventArgs e)
+        {
+            if (currHistoryIdx < navHistory.Count - 1)
+            {
+                currHistoryIdx++;
+                string path = navHistory[currHistoryIdx];
+                txt_path.Text = path;
+                LoadFileList(path);
+            }
+        }
+
+        //private void btn_go_Click(object sender, EventArgs e)
+        //{
+        //    string path = txt_path.Text;
+        //    if (Directory.Exists(path))
+        //    {
+        //        NavigateTo(path);
+        //    }
+        //    else
+        //    {
+        //        MessageBox.Show("The folder doesn't exist.");
+        //    }
+        //}
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            if (e.Node?.Tag is string path && Directory.Exists(path))
+            {
+                NavigateTo(path);
+            }
+        }
+
+        private void LoadFileList(string path)
+        {
+            try
+            {
+                listView1.Items.Clear();
+                var dirInfo = new DirectoryInfo(path);
+                foreach (var file in dirInfo.GetFiles())
+                {
+                    listView1.Items.Add(new ListViewItem(file.Name) { Tag = file.FullName });
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error loading file list: {ex.Message}");
+            }
+        }
+
+        private string GetFullPath(TreeNode node)
+        {
+            if (node.Parent == null)
+            {
+                return node.Tag as string;
+            }
+            else
+            {
+                return Path.Combine(GetFullPath(node.Parent), node.Tag as string);
+            }
         }
     }
 }
